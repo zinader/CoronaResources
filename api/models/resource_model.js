82b3575478c6d32d27dab104d0d03d6b9f078dda
Aditya Saxena <aditya.saxena33@gmail.com>
2021-04-20 17:38:31 +0530

    Initiated

--- api/models/resource_model.js
+++ api/models/resource_model.js
@@ -1,17 +1,20 @@
-const mongoose = require ('mongoose');
+import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
 //Resource information needed
-const resourceSchema = new Schema({
+const resourceSchema = new Schema(
+  {
     name: { type: String, required: true },
     description: { type: String, required: true },
-    typetags: { type: [String]},
+    typetags: { type: [String] },
     price: { type: Number, required: true },
-},{
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const Resource = mongoose.model('Resource', resourceSchema);
+const Resource = mongoose.model("Resource", resourceSchema);
 
-module.exports = Resource;
+export default Resource;
